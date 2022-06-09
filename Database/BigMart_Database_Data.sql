ae5f9faab24aa657ff39aa8c5793746907b52c8b
HoangCaoLong <longhc@dantri.net.vn>
2022-06-09 14:45:25 +0700

    Init

--- Database/BigMart_Database_Data.sql
+++ Database/BigMart_Database_Data.sql
@@ -1,187 +1,68 @@
-create database BigMart_DB
-go
+insert into tblKhachHang(ten,gioitinh,ngaysinh,dienthoai,diachi,email,ngaytao,ngaycapnhat,trangthai) values 
+('Nguyễn Ngọc Thúy',1,'1990-11-23','0832536199','Khu tập thể A3 Nam Đồng, A1P49, Phố P. Hồ Đắc Di, Nam Đồng, Đống Đa, Hà Nội','nguyenngocthuy@gmail.com','2022-06-09','',1),
+('Đáo Đức Thành',0,'1991-11-23','0832536200','Tòa nhà C´Land, 156 Ng. Xã Đàn 2, Nam Đồng, Đống Đa, Hà Nội','daoducthanh@gmail.com','2022-06-09','',0),
+('Tô Nghị',1,'1992-11-23','0832536201','Trần Hưng Đạo, Hoàn Kiếm, Hà Nội','tonghi@gmail.com','2022-06-09','',1),
+('Nguyễn Lê Tuấn',0,'1993-11-23','0832536202','81 Trần Hưng Đạo, Hoàn Kiếm, Hà Nội','nguyenletuan@gmail.com','2022-06-09','',0),
+('Trần Thị Thu',1,'1994-11-23','0832536203','6 Ng. 84 P. Trần Quang Diệu, Chợ Dừa, Đống Đa, Hà Nội','tranthithu@gmail.com','2022-06-09','',1),
+('Phạm Duy Hồng',0,'1995-11-23','0832536204','65 Nguyễn Đình Thi, Thuỵ Khuê, Tây Hồ, Hà Nội','phamduyhong@gmail.com','2022-06-09','',0),
+('Lê Hồng Nguyên',1,'1996-11-23','0832536205','51 Đ. Âu Cơ, Tứ Liên, Tây Hồ, Hà Nội','lehongnguyen@gmail.com','2022-06-09','',1),
+('Hồ Cung Đạt Nhân',0,'1997-11-23','0832536206','Hàng Bột, Đống Đa, Hà Nội','hocungdatnhan@gmail.com','2022-06-09','',0),
+('Chu Văn Nam',1,'1998-11-23','0832536207','9b Nguyễn Đình Thi, Thuỵ Khuê, Tây Hồ, Hà Nội','chuvannam@gmail.com','2022-06-09','',1),
+('Huỳnh Đức Long',0,'1999-11-23','0832536208','88 P.Yên Lãng, Láng Hạ, Đống Đa, Trung Hoà Cầu Giấy Hà Nội','huynhduclong@gmail.com','2022-06-09','',0);
 
-use BigMart_DB
-go
+insert into tblDiemThuong(idkhachhang,noidung,diem,ngaytao) values 
+(1,'Đây là phần quà dành cho bạn, cảm ơn đã mua hàng của chúng tôi',1,'2022-06-09'),
+(2,'Đây là phần quà dành cho bạn, cảm ơn đã mua hàng của chúng tôi',2,'2022-06-09'),
+(3,'Đây là phần quà dành cho bạn, cảm ơn đã mua hàng của chúng tôi',3,'2022-06-09'),
+(4,'Đây là phần quà dành cho bạn, cảm ơn đã mua hàng của chúng tôi',4,'2022-06-09'),
+(5,'Đây là phần quà dành cho bạn, cảm ơn đã mua hàng của chúng tôi',5,'2022-06-09'),
+(6,'Đây là phần quà dành cho bạn, cảm ơn đã mua hàng của chúng tôi',6,'2022-06-09'),
+(7,'Đây là phần quà dành cho bạn, cảm ơn đã mua hàng của chúng tôi',7,'2022-06-09'),
+(8,'Đây là phần quà dành cho bạn, cảm ơn đã mua hàng của chúng tôi',8,'2022-06-09'),
+(9,'Đây là phần quà dành cho bạn, cảm ơn đã mua hàng của chúng tôi',9,'2022-06-09'),
+(10,'Đây là phần quà dành cho bạn, cảm ơn đã mua hàng của chúng tôi',10,'2022-06-09');
 
-create table tblKhachHang
-(
-    id int primary key,
-    ho varchar(64) not null,
-    ten varchar(64) not null,
-    gioitinh tinyint default(1),
-    ngaysinh date check (ngaysinh > '1930-01-01'),
-    dienthoai varchar(11) not null unique,
-    diachi varchar(128),
-    email varchar(128) not null unique,
-    ngaytao date default(getDate()),
-    ngaycapnhat date check (ngaycapnhat < getDate()),
-    trangthai bit default(1)
-)
-go
+insert into tblCuaHang(tencuahang,diachi,sdt,idnhanvien,giomocua,giodongcua,ghichu) values 
+('Big Mart 1','Số 349 đường 3/2, TT. Đức Tài, H. Đức Linh, T. Bình Thuận (Đối diện bến xe Đức Tài)','0832536199',1,7,19,'Cửa hàng thứ nhất'),
+('Big Mart 2','Tiểu khu 7, Thị Trấn Thuận Châu, Huyện Thuận Châu, Tỉnh Sơn La','0832536200',2,7,19,'Cửa hàng thứ hai'),
+('Big Mart 3','Số 473, đường Tô Hiệu, TT. Ít Ong, H. Mường La, T. Sơn La','0832536201',3,7,19,'Cửa hàng thứ ba'),
+('Big Mart 4','Tổ 9, Phường Tân Phong, Thành Phố Lai Châu, Tỉnh Lai Châu','0832536202',4,7,19,'Cửa hàng thứ tư'),
+('Big Mart 5','Số 69 Đường Hồng Bàng, P. Sở Dầu, Q. Hồng Bàng, TP. Hải Phòng','0832536203',5,7,19,'Cửa hàng thứ năm'),
+('Big Mart 6','Quốc lộ 27, Thôn Hạnh Trí 1, X. Quảng Sơn, H. Ninh Sơn, T. Ninh Thuận (gần chợ Quảng Sơn)','0832536204',6,7,19,'Cửa hàng thứ sáu'),
+('Big Mart 7','Phố Ba Mỏ, TT. Thanh Sơn, H. Thanh Sơn, T. Phú Thọ (Gần ngã tư bến xe Thanh Sơn)','0832536205',7,7,19,'Cửa hàng thứ bảy'),
+('Big Mart 8','485 Hùng Vương, TT. Vạn Giã, H. Vạn Ninh, T. Khánh Hòa','0832536206',8,7,19,'Cửa hàng thứ tám'),
+('Big Mart 9','226 Trần Hưng Đạo, Phường Nam Lý, Thành phố Đồng Hới, Tỉnh Quảng Bình','0832536207',9,7,19,'Cửa hàng thứ chín'),
+('Big Mart 10','Quốc Lộ 1A, Thôn Dinh Mười, X. Gia Ninh, H. Quảng Ninh, T. Quảng Bình','0832536208',10,7,19,'Cửa hàng thứ mười');
 
-create table tblDiemThuong
-(
-    id int primary key,
-    idkhachhang int foreign key references tblKhachHang(id),
-    noidung varchar(128),
-    diem smallint,
-    ngaytao date default(getDate())
-)
-go
+insert into tblPhanQuyen(tenquyen,vitri,ngaytao,ghichu) values 
+();
 
-create table tblCuaHang
-(
-    id int primary key,
-    tencuahang varchar(128) not null,
-    diachi varchar(128) not null,
-    sdt1 varchar(11) not null unique,
-    sdt2 varchar(11),
-    idnhanvien int,
-    giomocua tinyint,
-    giodongcua tinyint,
-    ghichu varchar(256)
-)
-go
+insert into tblNhanVien(ten,gioitinh,ngaysinh,ngayvaolam,ngaycapnhat,sdt,email,diachi,idphanquyen,luongcoban,username,`password`,ghichu,trangthai) values 
+();
 
-create table tblPhanQuyen
-(
-    id int primary key,
-    tenquyen varchar(8) not null,
-    vitri varchar(64) not null,
-    ngaytao date default(getDate()),
-    ghichu varchar(256)
-)
-go
+insert into tblChungLoai(ten,mota,anhminhhoa,ngaytao,ngaycapnhat,trangthai) values 
+();
 
-create table tblNhanVien
-(
-    id int primary key,
-    hovaten varchar(64) not null,
-    gioitinh tinyint default(1),
-    ngaysinh date not null,
-    ngayvaolam date default(getDate()),
-    ngaycapnhat date,
-    didong varchar(11) not null unique,
-    dtgiadinh varchar(11),
-    email varchar(128) unique,
-    diachi varchar(128),
-    idphanquyen int foreign key references tblPhanQuyen(id),
-    luongcoban float default(0),
-    username varchar(64),
-    [password] varchar(64),
-    ghichu varchar(256),
-    trangthai bit default(1)
-)
-go
+insert into tblDonViTinh(tendonvi,ghichu) values 
+();
 
-create table tblChungLoai
-(
-    id int primary key,
-    ten varchar(64) not null,
-    mota varchar(128),
-    anhminhhoa varchar(256) not null,
-    ngaytao date,
-    ngaycapnhat date,
-    trangthai bit default (1)
-)
-go
+insert into tblNhaCungCap(tennhacc,diachi,sdt,email,ngaytao,ngaycapnhat,ghichu,trangthai) values 
+();
 
-create table tblDonViTinh
-(
-    id int primary key,
-    tendonvi varchar(32) not null,
-    ghichu varchar(256)
-)
-go
+insert into tblVanChuyen(ten,ghichu,trangthai) values 
+();
 
-create table tblNhaCungCap
-(
-    id int primary key,
-    tennhacc varchar(128) not null,
-    diachi varchar(128) not null,
-    sdt1 varchar(11) not null,
-    sdt2 varchar(11),
-    fax varchar(16),
-    email varchar(256) not null unique,
-    website varchar(256) not null unique,
-    ngaytao date default(getDate()),
-    ngaycapnhat date,
-    taxcode varchar(64), -- Mã số thuế
-    ghichu varchar(256),
-    trangthai bit default(1)
-)
-go
+insert into tblThanhToan(ten,ghichu,trangthai) values 
+();
 
+insert into tblHoaDon(ngaytao,ngaygiaohang,idvanchuyen,idthanhtoan,idkhachhang,idnhanvien,trangthai,diachinhan,sdtnhan,ghichu) values 
+();
 
+insert into tblHangHoa(ten,idchungloai,iddonvitinh,idnhacc,soluong,gianhap,giaxuat,ngaytao,ngaycapnhat,ghichu) values 
+();
 
-create table tblVanCHuyen
-(
-    id int primary key,
-    ten varchar(64),
-    ghichu varchar(128),
-    trangthai bit default(1)
-)
-go
+insert into tblHoaDonChiTiet(idhoadon,idhanghoa,soluong,giaban) values 
+();
 
-create table tblThanhToan
-(
-    id int primary key,
-    ten varchar(64),
-    ghichu varchar(128),
-    trangthai bit default(1)
-)
-go
-
-create table tblHoaDon
-(
-    id int primary key,
-    ngaytao datetime default(getDate()),
-    ngaygiaohang datetime default(getDate()),
-    idvanchuyen int foreign key references tblVanCHuyen(id),
-    idthanhtoan int foreign key references tblThanhToan(id),
-    idkhachhang int foreign key references tblKhachHang(id),
-    idnhanvien int foreign key references tblNhanVien(id),
-    trangthai bit default(1),
-    diachinhan varchar(128) not null,
-    sdtnhan varchar(11) not null ,
-    ghichu varchar(128)
-)
-go
-
-create table tblHangHoa
-(
-    id int primary key,
-    barcode varchar(32) not null, -- Mã vạch
-    tenhanghoa varchar(128) not null,
-    idchungloai int foreign key references tblChungLoai(id),
-    iddonvitinh int foreign key references tblDonViTinh(id),
-    idnhacc int foreign key references tblNhaCungCap(id),
-    soluong float default(0),
-    gianhap float default(0),
-    giaxuat float default(0),
-    ngaytao date default(getDate()),
-    ngaycapnhat date,
-    ghichu varchar(128)
-)
-go
-
-create table tblHoaDonChiTiet
-(
-    idhoadon int foreign key references tblHoaDon(id),
-    idhanghoa int foreign key references tblHangHoa(id),
-    soluong float check(soluong>0),
-    gia float check(gia>=0), -- Giá bán
-    primary key(idhoadon, idhanghoa)
-)
-go
-
-create table tblHinhAnh
-(
-    id int primary key,
-    idhanghoa int foreign key references tblHangHoa(id),
-    ghichu varchar(64),
-    stt tinyint, -- Thứ tự ảnh
-    trangthai bit default(1)
-)
-go
+insert into tblHinhAnh(idhanghoa,ghichu,sothutu,trangthai) values 
+();
--- Database/BigMart_Database_Init.sql
+++ Database/BigMart_Database_Init.sql
@@ -1,187 +1,173 @@
-create database BigMart_DB
-go
+create database BigMart_DB;
 
-use BigMart_DB
-go
+use BigMart_DB;
 
 create table tblKhachHang
 (
-    id int primary key,
-    ho varchar(64) not null,
+    id int auto_increment primary key,
     ten varchar(64) not null,
     gioitinh tinyint default(1),
-    ngaysinh date check (ngaysinh > '1930-01-01'),
+    ngaysinh date,
     dienthoai varchar(11) not null unique,
     diachi varchar(128),
     email varchar(128) not null unique,
-    ngaytao date default(getDate()),
-    ngaycapnhat date check (ngaycapnhat < getDate()),
+    ngaytao date,
+    ngaycapnhat date,
     trangthai bit default(1)
-)
-go
+);
 
 create table tblDiemThuong
 (
-    id int primary key,
-    idkhachhang int foreign key references tblKhachHang(id),
+    id int auto_increment primary key,
+    idkhachhang int,
     noidung varchar(128),
     diem smallint,
-    ngaytao date default(getDate())
-)
-go
+    ngaytao date,
+    foreign key (idkhachhang) references tblKhachHang(id)
+);
 
 create table tblCuaHang
 (
-    id int primary key,
+    id int auto_increment primary key,
     tencuahang varchar(128) not null,
     diachi varchar(128) not null,
-    sdt1 varchar(11) not null unique,
-    sdt2 varchar(11),
+    sdt varchar(11) not null unique,
     idnhanvien int,
     giomocua tinyint,
     giodongcua tinyint,
     ghichu varchar(256)
-)
-go
+);
 
 create table tblPhanQuyen
 (
-    id int primary key,
+    id int auto_increment primary key,
     tenquyen varchar(8) not null,
     vitri varchar(64) not null,
-    ngaytao date default(getDate()),
+    ngaytao date,
     ghichu varchar(256)
-)
-go
+);
 
 create table tblNhanVien
 (
-    id int primary key,
-    hovaten varchar(64) not null,
+    id int auto_increment primary key,
+    ten varchar(64) not null,
     gioitinh tinyint default(1),
     ngaysinh date not null,
-    ngayvaolam date default(getDate()),
+    ngayvaolam date,
     ngaycapnhat date,
-    didong varchar(11) not null unique,
-    dtgiadinh varchar(11),
+    sdt varchar(11) not null unique,
     email varchar(128) unique,
     diachi varchar(128),
-    idphanquyen int foreign key references tblPhanQuyen(id),
+    idphanquyen int,
     luongcoban float default(0),
     username varchar(64),
-    [password] varchar(64),
+    `password` varchar(64),
     ghichu varchar(256),
-    trangthai bit default(1)
-)
-go
+    trangthai bit default(1),
+    foreign key (idphanquyen) references tblPhanQuyen(id)
+);
 
 create table tblChungLoai
 (
-    id int primary key,
+    id int auto_increment primary key,
     ten varchar(64) not null,
     mota varchar(128),
     anhminhhoa varchar(256) not null,
     ngaytao date,
     ngaycapnhat date,
     trangthai bit default (1)
-)
-go
+);
 
 create table tblDonViTinh
 (
-    id int primary key,
+    id int auto_increment primary key,
     tendonvi varchar(32) not null,
     ghichu varchar(256)
-)
-go
+);
 
 create table tblNhaCungCap
 (
-    id int primary key,
+    id int auto_increment primary key,
     tennhacc varchar(128) not null,
     diachi varchar(128) not null,
-    sdt1 varchar(11) not null,
-    sdt2 varchar(11),
-    fax varchar(16),
+    sdt varchar(11) not null,
     email varchar(256) not null unique,
-    website varchar(256) not null unique,
-    ngaytao date default(getDate()),
+    ngaytao date,
     ngaycapnhat date,
-    taxcode varchar(64), -- Mã số thuế
     ghichu varchar(256),
     trangthai bit default(1)
-)
-go
-
-
+);
 
 create table tblVanCHuyen
 (
-    id int primary key,
+    id int auto_increment primary key,
     ten varchar(64),
     ghichu varchar(128),
     trangthai bit default(1)
-)
-go
+);
 
 create table tblThanhToan
 (
-    id int primary key,
+    id int auto_increment primary key,
     ten varchar(64),
     ghichu varchar(128),
     trangthai bit default(1)
-)
-go
+);
 
 create table tblHoaDon
 (
-    id int primary key,
-    ngaytao datetime default(getDate()),
-    ngaygiaohang datetime default(getDate()),
-    idvanchuyen int foreign key references tblVanCHuyen(id),
-    idthanhtoan int foreign key references tblThanhToan(id),
-    idkhachhang int foreign key references tblKhachHang(id),
-    idnhanvien int foreign key references tblNhanVien(id),
+    id int auto_increment primary key,
+    ngaytao datetime,
+    ngaygiaohang datetime,
+    idvanchuyen int,
+    idthanhtoan int,
+    idkhachhang int,
+    idnhanvien int ,
     trangthai bit default(1),
     diachinhan varchar(128) not null,
     sdtnhan varchar(11) not null ,
-    ghichu varchar(128)
-)
-go
+    ghichu varchar(128),
+    foreign key (idvanchuyen) references tblVanCHuyen(id),
+    foreign key (idthanhtoan) references tblThanhToan(id),
+    foreign key (idkhachhang) references tblKhachHang(id),
+    foreign key (idnhanvien) references tblNhanVien(id)
+);
 
 create table tblHangHoa
 (
-    id int primary key,
-    barcode varchar(32) not null, -- Mã vạch
-    tenhanghoa varchar(128) not null,
-    idchungloai int foreign key references tblChungLoai(id),
-    iddonvitinh int foreign key references tblDonViTinh(id),
-    idnhacc int foreign key references tblNhaCungCap(id),
+    id int auto_increment primary key,
+    ten varchar(128) not null,
+    idchungloai int,
+    iddonvitinh int,
+    idnhacc int,
     soluong float default(0),
     gianhap float default(0),
     giaxuat float default(0),
-    ngaytao date default(getDate()),
+    ngaytao date,
     ngaycapnhat date,
-    ghichu varchar(128)
-)
-go
+    ghichu varchar(128),
+    foreign key (idchungloai) references tblChungLoai(id),
+    foreign key (iddonvitinh) references tblDonViTinh(id),
+    foreign key (idnhacc) references tblNhaCungCap(id)
+);
 
 create table tblHoaDonChiTiet
 (
-    idhoadon int foreign key references tblHoaDon(id),
-    idhanghoa int foreign key references tblHangHoa(id),
-    soluong float check(soluong>0),
-    gia float check(gia>=0), -- Giá bán
-    primary key(idhoadon, idhanghoa)
-)
-go
+    idhoadon int,
+    idhanghoa int,
+    soluong float,
+    giaban float,
+    primary key(idhoadon, idhanghoa),
+    foreign key (idhoadon) references tblHoaDon(id),
+    foreign key (idhanghoa) references tblHangHoa(id)
+);
 
 create table tblHinhAnh
 (
-    id int primary key,
-    idhanghoa int foreign key references tblHangHoa(id),
+    id int auto_increment primary key,
+    idhanghoa int,
     ghichu varchar(64),
-    stt tinyint, -- Thứ tự ảnh
-    trangthai bit default(1)
-)
-go
+    sothutu tinyint,
+    trangthai bit default(1),
+    foreign key (idhanghoa) references tblHangHoa(id)
+);
